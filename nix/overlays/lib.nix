43823a4998b3273e0defa087d97aa0072c60cc37
Drew Hess <src@drewhess.com>
2020-03-23 16:43:58 +0000

    Add a helpful `cleanSource` filter for Haskell projects.
    Use this rather than `haskell-nix.cleanGit` for better interactive
    development ergonomics, as it doesn't require that sources files be in
    the git index.

--- nix/overlays/lib.nix
+++ nix/overlays/lib.nix
@@ -31,6 +31,28 @@ let
   hacknixShell865 = hacknixShell super.haskell-hacknix.ghc865;
   hacknixShell883 = hacknixShell super.haskell-hacknix.ghc883;
 
+
+  # A useful source cleaner for Haskell projects.
+  #
+  # The advantage of this over haskell-nix.cleanGit is that the latter
+  # requires that files are in the git index, which isn't helpful
+  # while developing a package.
+
+  cleanSource = { src, excludeDirs ? [], excludeFiles ? [] }:
+    let
+      filter = name: type:
+        let baseName = baseNameOf (toString name);
+        in ! (
+          (type != "directory" && builtins.elem name excludeFiles) ||
+          (type == "directory" && builtins.elem name excludeDirs)
+        );
+      cleanSource' = src': super.lib.sources.cleanSourceWith {
+        inherit filter;
+        src = src';
+      };
+    in
+      cleanSource' (super.lib.sources.cleanSourceAllExtraneous src);
+
 in
 {
   lib = (super.lib or {}) // {
@@ -38,6 +60,7 @@ in
       haskellLib = (super.lib.hacknix.haskellLib or {}) // {
         inherit collectTests collectChecks;
         inherit hacknixShell hacknixShell865 hacknixShell883;
+        inherit cleanSource;
       };
     };
   };
