ac437ec33f9636e4cced81f38e9d2cf562597334
Drew Hess <src@drewhess.com>
2020-03-26 10:51:48 +0000

    Switch to cabal-doctest.
    Still not working:
    
    Preprocessing test suite 'doctest' for hhp-1.0.0.0..
    Building test suite 'doctest' for hhp-1.0.0.0..
    [1 of 1] Compiling Main
    
    test/doctest.hs:3:1: error:
        Could not find module ‘Build_doctests’
        Use -v to see a list of the files searched for.
      |
    3 | import Build_doctests (flags, pkgs, module_sources)
      | ^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^
    
    builder for '/nix/store/qif95rb421jx04h8lqz32zd0r6r8p6nj-hhp-1.0.0.0-test-doctest.drv' failed with exit code 1
    cannot build derivation '/nix/store/f5jxxlwkc3i937bibql16prmfvb4glh0-hhp-1.0.0.0-test-doctest-check.drv': 1 dependencies couldn't be built
    error: build of '/nix/store/f5jxxlwkc3i937bibql16prmfvb4glh0-hhp-1.0.0.0-test-doctest-check.drv', '/nix/store/mhdlcdnzac0r2rpk7h63p6260z2p0bjl-hhp-1.0.0.0-test-hhp-test-check.drv', '/nix/store/mzs4z1iq8ha5pw9rac6sy3gkq9a5kdnp-hhp-1.0.0.0-test-hlint-check.drv' failed

--- hhp/test/doctest.hs
+++ hhp/test/doctest.hs
@@ -1,1 +1,12 @@
-{-# OPTIONS_GHC -F -pgmF doctest-driver-gen -optF src #-}
+module Main where
+
+import Build_doctests (flags, pkgs, module_sources)
+import Data.Foldable (traverse_)
+import Test.DocTest (doctest)
+
+main :: IO ()
+main = do
+    traverse_ putStrLn args
+    doctest args
+  where
+    args = flags ++ pkgs ++ module_sources
