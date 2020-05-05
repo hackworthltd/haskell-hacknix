a18cd28d2ccee9ff0a7c7fad87c031b751e203ac
Drew Hess <src@drewhess.com>
2020-05-05 17:58:28 +0100

    cabal-fmt: fixes for GHC 8.8.3.

--- nix/pkgs/cabal-fmt.nix
+++ nix/pkgs/cabal-fmt.nix
@@ -14,7 +14,16 @@ let
     in haskell-nix.cabalProject {
       src = localLib.sources.cabal-fmt;
 
-      pkg-def-extras = [];
+      pkg-def-extras = [
+        (
+          hackage: {
+            binary = hackage.binary."0.8.8.0".revisions.default;
+            parsec = hackage.parsec."3.1.14.0".revisions.default;
+            text = hackage.text."1.2.4.0".revisions.default;
+            time = hackage.time."1.9.3".revisions.default;
+          }
+        )
+      ];
 
       modules = [
         {
