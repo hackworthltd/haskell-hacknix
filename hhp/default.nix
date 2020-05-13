58e4eade932879eaed6224febb6efbdbd122e19a
Drew Hess <src@drewhess.com>
2020-05-14 00:20:47 +0100

    Reformat hhp/default.nix; no functional changes.

--- hhp/default.nix
+++ hhp/default.nix
@@ -2,9 +2,12 @@
 let
   inherit (pkgs.lib.hacknix.haskellLib)
     cleanSource collectTests collectChecks filterByPrefix;
+
   inherit (pkgs.haskell-hacknix)
     ghc865 ghc883 cache cabalProject865 cabalProject883;
+
   hhpSrc = pkgs.gitignoreSource ../.;
+
   mkHhpPackages = { profiling ? false }: pkgs.recurseIntoAttrs {
     ghc865 = cabalProject865 {
       src = hhpSrc;
@@ -21,21 +24,27 @@ let
         [ (hackage: { alex = hackage.alex."3.2.5".revisions.default; }) ];
     };
   };
+
   hhpPackage = filterByPrefix "hhp";
   hhpPackages = mkHhpPackages { };
   hhpPackagesProfiled = mkHhpPackages { profiling = true; };
+
   haskellPackages = hhpPackages.ghc865;
   shell = ghc865.shellFor haskellPackages { };
   cachedShell = cache shell;
+
   haskellPackagesProfiled = hhpPackagesProfiled.ghc865;
   shellProfiled = ghc865.shellFor haskellPackagesProfiled { };
   cachedShellProfiled = cache shellProfiled;
+
   haskellPackages883 = hhpPackages.ghc883;
   shell883 = ghc883.shellFor haskellPackages883 { };
   cachedShell883 = cache shell883;
+
   haskellPackages883Profiled = hhpPackagesProfiled.ghc883;
   shell883Profiled = ghc883.shellFor haskellPackages883Profiled { };
   cachedShell883Profiled = cache shell883Profiled;
+
   self = {
     ## GHC 8.6.5.
     inherit haskellPackages shell;
